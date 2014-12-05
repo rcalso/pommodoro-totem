8f7fdf514b7931ebdc93336228a6b828ae5e76e6
rcalso <ramon.calso@medianet.es>
2014-12-05 21:31:53 +0100

    Add Comments and HIGH/LOW Constants

--- DontDisturb_RelaxTime/DontDisturb_RelaxTime.ino
+++ DontDisturb_RelaxTime/DontDisturb_RelaxTime.ino
@@ -11,20 +11,20 @@ void setup() {
 
 void loop() {
   dontDisturb();
-  delay(5000);
+  delay(5000); // wait for 5 seconds
   relaxTime();
-  delay(5000);
+  delay(5000); // wait for 5 seconds
 }
 
 void dontDisturb(){
   Serial.print("Don't disturb me!!! I'm busy");
-  digitalWrite(LED_RED, 1);
-  digitalWrite(LED_GREEN, 0);
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, LOW);
 }
 void relaxTime(){
   Serial.print("Relax Time!! ");
-  digitalWrite(LED_RED, 0);
-  digitalWrite(LED_GREEN, 1);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
 }
 
 
